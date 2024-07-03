34b89ee3d2d71f1d037b3735ac9a8bb4984ce6dc
nicholas-ananias <nicholas.ananias@gmail.com>
2024-07-02 23:21:40 -0300

    update

--- curso/models.py
+++ curso/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+CATEGORIAS = (
+    ("categoria1", "Categoria 1"),
+    ("categoria2", "Categoria 2"),
+    ("categoria3", "Categoria 3")
+)
+
+class Curso(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_cursos')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=20, choices=CATEGORIAS)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    visualizacoes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.titulo
+
+    #titulo, descricao, categoria, data_de_criacao, visualizacoes
